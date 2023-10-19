4381a60f47caba2133c4df58d35c51f0ddbddef1
Joshua Schuessler <60977809+BlueSchues@users.noreply.github.com>
2023-10-19 10:25:05 -0500

    Removed printout and started to code visualization

--- lotto.py
+++ lotto.py
@@ -1,9 +1,6 @@
 # importing necessary package
 import pandas as pd
 
-# Mav number of different types of cards requested
-games = 3
-
 # Reading scratch ticket data from website into a dataframe
 df = pd.read_csv('https://www.texaslottery.com/export/sites/' \
                  'lottery/Games/Scratch_Offs/scratchoff.csv',
@@ -16,15 +13,5 @@ df = df[df['Prize Level'] != 'TOTAL']
 df['Prize Level'] = pd.to_numeric(df['Prize Level'])
 df['Prizes Claimed'] = df['Prizes Claimed'].fillna(0).astype(int)
 
-# Setting index to better organize data
-df = df.set_index(['Game Number', 'Prize Level'])
-
 # Creating Claim % as target variable
 df['Claim %'] = (df['Prizes Claimed']/df['Total Prizes in Level']*100).round(2)
-
-# Finding the smallest medians
-median = df['Claim %'].groupby('Game Number').median().sort_values()[:games]
-
-# Printing the claims in a clean way
-for i in range(games):
-    print(df[df.index.get_level_values(0) == median.index[i]])
---
+++ visualize.py
@@ -0,0 +1,14 @@
+# importing neccessary packages
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# importing data from lotto script
+from lotto import df
+
+fig, ax = plt.subplots()
+
+# Plotting individual graphs
+
+plt.show()
