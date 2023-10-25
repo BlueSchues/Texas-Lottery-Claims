a1de067210a39b632f47852200a60487ab0cdaea
Joshua Schuessler <60977809+BlueSchues@users.noreply.github.com>
2023-10-25 12:13:21 -0500

    Added preliminary visualization

--- visualize.py
+++ visualize.py
@@ -2,13 +2,16 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 # importing data from lotto script
 from lotto import df
 
 fig, ax = plt.subplots()
 
-# Plotting individual graphs
+games = list(df['Game Number'].unique())
+game = df[df['Game Number'] == games[-1]]
 
+ax.bar(game['Prize Level'], game['Claim %'])
+ax.set(xlim=(0,100), ylim=(0,100))
+print(game['Claim %'].head())
 plt.show()
